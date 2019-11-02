415d842c8cd36331d76d0a1c4011a69f1d794f83
A_A <21040751+Otto-AA@users.noreply.github.com>
2019-11-02 16:01:20 +0100

    Split into LocalPod and LocalPodManager

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-const httpsLocalhost = require("https-localhost")()
 const LocalPod = require('./LocalPod')
-const SolidRest = require('solid-rest')
-const FileHandler = require('solid-rest/src/file')
-const solidRest = new SolidRest([new FileHandler])
-const solidFileFetch = require('./solidFileFetch')
 
-async function main() {
-    // TODO: Check if there is a better way to run localhost via https
-    const certs = await httpsLocalhost.getCerts()
-    const fetch = solidRest.fetch.bind(solidRest)
-
-    new LocalPod({
-        port: 3000,
-        basePath: '/home/a_a/test',
-        certs,
-        fetch: solidFileFetch
-    }).startListening()
-
-    new LocalPod({
-        port: 4000,
-        basePath: '/home/a_a/test',
-        certs,
-        fetch,
-        prefix: 'file://'
-    }).startListening()
-}
-main()
+module.exports = LocalPod
